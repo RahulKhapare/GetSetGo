af08ba071e1f0d232939a1f9003cd178c55cca70
RAKDeveloper <62135897+RAKDeveloper@users.noreply.github.com>
2020-12-07 12:11:39 +0530

    init commit

---
+++ app/src/main/java/com/getsetgo/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.getsetgo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.getsetgo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
