a72ecb21ba632ae941bcee5d2554663dc0b80b24
muhammadjund <ansjuned72@gmail.com>
2020-12-12 20:04:50 +0530

    First Commit

--- app/src/main/java/com/getsetgo/Adapter/CurriculumLectureAdapter.java
+++ app/src/main/java/com/getsetgo/Adapter/CurriculumLectureAdapter.java
@@ -1,9 +1,13 @@
 package com.getsetgo.Adapter;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
@@ -22,10 +26,14 @@ public class CurriculumLectureAdapter extends RecyclerView.Adapter<CurriculumLec
 
     Context context;
     int Count;
+    Animation animUp,animDown;
 
     public CurriculumLectureAdapter(Context context, int count) {
         this.context = context;
         this.Count = count;
+
+        animUp = AnimationUtils.loadAnimation(context, R.anim.slide_up);
+        animDown = AnimationUtils.loadAnimation(context, R.anim.slide_down);
 
     }
 
@@ -44,12 +52,14 @@ public class CurriculumLectureAdapter extends RecyclerView.Adapter<CurriculumLec
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
                     holder.rlCollapse.setVisibility(View.VISIBLE);
+                    holder.rlCollapse.startAnimation(animDown);
                     holder.recyclerCollapsed.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
                     LecturePreviewAdapter lecturePreviewAdapter = new LecturePreviewAdapter(context);
                     holder.recyclerCollapsed.setItemAnimator(new DefaultItemAnimator());
                     holder.recyclerCollapsed.setAdapter(lecturePreviewAdapter);
                     lecturePreviewAdapter.notifyDataSetChanged();
                 } else {
+                    holder.rlCollapse.startAnimation(animUp);
                     holder.rlCollapse.setVisibility(View.GONE);
                 }
             }
