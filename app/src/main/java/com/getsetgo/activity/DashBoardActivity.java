55451b6ed587c6355a139905d3e29898e1aa0d48
muhammadjund <ansjuned72@gmail.com>
2020-12-14 14:42:04 +0530

    First Commit

---
+++ app/src/main/java/com/getsetgo/activity/DashBoardActivity.java
@@ -0,0 +1,16 @@
+package com.getsetgo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.getsetgo.R;
+
+public class DashBoardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dash_board);
+    }
+}
